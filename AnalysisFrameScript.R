bbbfdc537b6499ba400db5c7a1db74e259dbe772
ltoker <lilchpilch@yyahoo.com>
2020-12-09 12:09:20 +0100

    Setting the environment and running scripts in order

---
+++ AnalysisFrameScript.R
@@ -0,0 +1,21 @@
+BiocManager::install("devtools")
+library(devtools)
+source_url("https://github.com/ltoker/GeneralRscripts/blob/main/generalFunc.R?raw=T")
+
+ResultsPath = "Results"
+
+if(!ResultsPath %in% list.dirs(full.names = F)){
+  dir.create(ResultsPath)
+}
+
+ResultsPath = paste0(ResultsPath, "/")
+
+
+#Preparing the data for analysis
+source("ProjectScripts/PrepareData.R")
+
+#Calculations and plotting of descriptive epidemiology - incidence, prevalence and mortality
+source("ProjectScripts/EpiPlotting.R")
+
+#Survival analysis - Kaplan-Meier
+source("ProjectScripts/SurvivalAnalyses.R")
